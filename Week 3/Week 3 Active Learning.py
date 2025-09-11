cf6269e522adac6f878b024838ee122f03fe36c2
egordi <egordi@sas.upenn.edu>
2025-09-11 16:11:15 -0400

    working on week 3 Active learning

--- Week 3/Week 3 Active Learning.py
+++ Week 3/Week 3 Active Learning.py
@@ -7,15 +7,18 @@ def is_population_disabled ( disabled: float, total_population : float) -> bool:
     else:
         return True
 
-print(is_population_disabled (disabled=0, total_population=32))
-print(is_population_disabled (disabled=52, total_population=392))
+#print(is_population_disabled (disabled=0, total_population=32))
+#print(is_population_disabled (disabled=52, total_population=392))
 
 
-def is_gp_religious_or_academic (Gathering_point : str):
-    gathering_point= set(['Mosque, Church, School, Institute, Education, Faculty'])
-    if 'Church' in is_gp_religious_or_academic():
+def is_gp_religious_or_academic (gp):
+    if 'Mosque' or 'Church' in gp:
+        return True
+    elif 'School' or 'Institute' or 'Education' or 'Faculty' in gp:
         return True
     else:
         return False
 
-print(is_gp_religious_or_academic('Almorada Church'))
+#print(is_gp_religious_or_academic('Faculty Of Earth Sciences and Mining'))
+#print(is_gp_religious_or_academic('Almorada Church'))
+print(is_gp_religious_or_academic('Health Insulation Building'))
