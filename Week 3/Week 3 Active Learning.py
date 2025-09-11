2c455d28499a7cd5960c9ed7dac07382cbec10a3
egordi <egordi@sas.upenn.edu>
2025-09-11 17:14:09 -0400

    working on week 3 Active learning

--- Week 3/Week 3 Active Learning.py
+++ Week 3/Week 3 Active Learning.py
@@ -1,5 +1,5 @@
 
-
+def is_min_ratio_toilets_to_people_met
 
 def is_population_disabled ( disabled: float, total_population : float) -> bool:
     if disabled / total_population < 0.1:
@@ -12,13 +12,21 @@ def is_population_disabled ( disabled: float, total_population : float) -> bool:
 
 
 def is_gp_religious_or_academic (gp):
-    if 'Mosque' or 'Church' in gp:
+    if 'Mosque' in gp:
         return True
-    elif 'School' or 'Institute' or 'Education' or 'Faculty' in gp:
+    elif 'Church' in gp:
+        return True
+    elif 'School' in gp:
+        return True
+    elif 'Institute' in gp:
+        return True
+    elif 'Education' in gp:
+        return True
+    elif 'Faculty' in gp:
         return True
     else:
         return False
 
-#print(is_gp_religious_or_academic('Faculty Of Earth Sciences and Mining'))
-#print(is_gp_religious_or_academic('Almorada Church'))
+print(is_gp_religious_or_academic('Faculty Of Earth Sciences and Mining'))
+print(is_gp_religious_or_academic('Almorada Church'))
 print(is_gp_religious_or_academic('Health Insulation Building'))
